da4919ac124cc6b6d3a033359af6ec249d65ffca
Frank Wang <yf.frank.wang@outlook.com>
2020-11-14 11:17:03 +0900

    logfmt: change formatter interface to type func

--- log/logfmt.go
+++ log/logfmt.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -13,11 +14,7 @@ type Formatter interface {
 
 // TextFormatter text formatter implement struct
 type TextFormatter struct {
-	fmts []formatter
-}
-
-type formatter interface {
-	format(sb *strings.Builder, le *Event)
+	fmts []fmtfunc
 }
 
 // NewFormatter create a Formatter instance
@@ -45,6 +42,7 @@ var FormatterSimple = NewFormatter("[%l] %m")
 // FormatterDefault default log format "%d{2006-01-02T15:04:05.000} %l %S:%L %F() - %m%T%n"
 var FormatterDefault = NewFormatter("%d{2006-01-02T15:04:05.000} %l %S:%L %F() - %m%T%n")
 
+var eol = geteol()
 var lvlPrefixs = [LevelTrace + 1]string{"N", "F", "E", "W", "I", "D", "T"}
 var lvlStrings = [LevelTrace + 1]string{"NONE ", "FATAL", "ERROR", "WARN ", "INFO ", "DEBUG", "TRACE"}
 
@@ -52,7 +50,7 @@ var lvlStrings = [LevelTrace + 1]string{"NONE ", "FATAL", "ERROR", "WARN ", "INF
 func (tf *TextFormatter) Format(le *Event) string {
 	sb := strings.Builder{}
 	for _, f := range tf.fmts {
-		f.format(&sb, le)
+		f(&sb, le)
 	}
 	return sb.String()
 }
@@ -68,7 +66,7 @@ func (tf *TextFormatter) Init(format string) {
 
 		// string
 		if s < i {
-			tf.fmts = append(tf.fmts, &strfmt{format[s:i]})
+			tf.fmts = append(tf.fmts, strfmt(format[s:i]))
 		}
 
 		i++
@@ -78,32 +76,32 @@ func (tf *TextFormatter) Init(format string) {
 		}
 
 		// symbol
-		var fmt formatter
+		var fmt fmtfunc
 		switch format[i] {
 		case 'c':
-			fmt = &namefmt{}
+			fmt = namefmt
 		case 'p':
-			fmt = &lvlpfmt{}
+			fmt = lvlpfmt
 		case 'l':
-			fmt = &lvlsfmt{}
+			fmt = lvlsfmt
 		case 'm':
-			fmt = &msgfmt{}
+			fmt = msgfmt
 		case 'n':
-			fmt = &eolfmt{}
+			fmt = eolfmt
 		case 'F':
-			fmt = &funcfmt{}
+			fmt = funcfmt
 		case 'S':
-			fmt = &filefmt{}
+			fmt = filefmt
 		case 'L':
-			fmt = &linefmt{}
+			fmt = linefmt
 		case 'T':
-			fmt = &tracefmt{}
+			fmt = tracefmt
 		case 'd':
 			p := format[i+1:]
 			if len(p) > 0 && p[0] == '{' {
 				e := strings.IndexByte(p, '}')
 				if e > 0 {
-					fmt = &timefmt{p[1:e]}
+					fmt = timefmt(p[1:e])
 					i += e + 1
 				}
 			}
@@ -112,7 +110,7 @@ func (tf *TextFormatter) Init(format string) {
 			if len(p) > 0 && p[0] == '{' {
 				e := strings.IndexByte(p, '}')
 				if e > 0 {
-					fmt = &paramfmt{p[1:e]}
+					fmt = paramfmt(p[1:e])
 					i += e + 1
 				}
 			}
@@ -125,93 +123,69 @@ func (tf *TextFormatter) Init(format string) {
 	}
 
 	if s < len(format) {
-		tf.fmts = append(tf.fmts, &strfmt{format[s:]})
+		tf.fmts = append(tf.fmts, strfmt(format[s:]))
 	}
 }
 
-type strfmt struct {
-	s string
+type fmtfunc func(sb *strings.Builder, le *Event)
+
+func geteol() string {
+	if runtime.GOOS == "windows" {
+		return "\r\n"
+	}
+	return "\n"
 }
 
-func (f *strfmt) format(sb *strings.Builder, le *Event) {
-	sb.WriteString(f.s)
+func strfmt(s string) fmtfunc {
+	return func(sb *strings.Builder, le *Event) {
+		sb.WriteString(s)
+	}
 }
 
-type lvlpfmt struct {
-}
-
-func (f *lvlpfmt) format(sb *strings.Builder, le *Event) {
+func lvlpfmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(lvlPrefixs[le.Level])
 }
 
-type lvlsfmt struct {
-}
-
-func (f *lvlsfmt) format(sb *strings.Builder, le *Event) {
+func lvlsfmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(lvlStrings[le.Level])
 }
 
-type namefmt struct {
-}
-
-func (f *namefmt) format(sb *strings.Builder, le *Event) {
+func namefmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(le.Logger.GetName())
 }
 
-type msgfmt struct {
-}
-
-func (f *msgfmt) format(sb *strings.Builder, le *Event) {
+func msgfmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(le.Msg)
 }
 
-type eolfmt struct {
+func eolfmt(sb *strings.Builder, le *Event) {
+	sb.WriteString(eol)
 }
 
-func (f *eolfmt) format(sb *strings.Builder, le *Event) {
-	sb.WriteString("\n")
-}
-
-type funcfmt struct {
-}
-
-func (f *funcfmt) format(sb *strings.Builder, le *Event) {
+func funcfmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(le.Func)
 }
 
-type filefmt struct {
-}
-
-func (f *filefmt) format(sb *strings.Builder, le *Event) {
+func filefmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(le.File)
 }
 
-type linefmt struct {
-}
-
-func (f *linefmt) format(sb *strings.Builder, le *Event) {
+func linefmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(strconv.Itoa(le.Line))
 }
 
-type tracefmt struct {
-}
-
-func (f *tracefmt) format(sb *strings.Builder, le *Event) {
+func tracefmt(sb *strings.Builder, le *Event) {
 	sb.WriteString(le.Trace)
 }
 
-type timefmt struct {
-	layout string
+func timefmt(layout string) fmtfunc {
+	return func(sb *strings.Builder, le *Event) {
+		sb.WriteString(le.When.Format(layout))
+	}
 }
 
-func (f *timefmt) format(sb *strings.Builder, le *Event) {
-	sb.WriteString(le.When.Format(f.layout))
-}
-
-type paramfmt struct {
-	key string
-}
-
-func (f *paramfmt) format(sb *strings.Builder, le *Event) {
-	sb.WriteString(fmt.Sprint(le.Logger.GetParam(f.key)))
+func paramfmt(key string) fmtfunc {
+	return func(sb *strings.Builder, le *Event) {
+		sb.WriteString(fmt.Sprint(le.Logger.GetParam(key)))
+	}
 }

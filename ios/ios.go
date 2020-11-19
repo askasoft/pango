21a0ee4c1010a5dc2dc5ea773e22c4813ea07302
Frank Wang <yf.frank.wang@outlook.com>
2020-11-19 10:50:49 +0900

    add ios

---
+++ ios/ios.go
@@ -0,0 +1,47 @@
+package ios
+
+import (
+	"fmt"
+	"os"
+)
+
+// FileExists check if file exists
+func FileExists(path string) error {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() {
+		return fmt.Errorf("%q is directory", path)
+	}
+	return nil
+}
+
+// FileReader a file reader
+type FileReader struct {
+	Path string
+	file *os.File
+}
+
+// Read implements io.Reader
+func (fr *FileReader) Read(p []byte) (n int, err error) {
+	if fr.file == nil {
+		file, err := os.Open(fr.Path)
+		if err != nil {
+			return 0, err
+		}
+		fr.file = file
+	}
+	return fr.Read(p)
+}
+
+// Close implements io.Close
+func (fr *FileReader) Close() error {
+	if fr.file == nil {
+		return nil
+	}
+
+	err := fr.file.Close()
+	fr.file = nil
+	return err
+}

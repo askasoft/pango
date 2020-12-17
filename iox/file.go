a68596b59a9c276517578abb41092317a1eedff8
Frank Wang <yf.frank.wang@outlook.com>
2020-12-17 15:25:51 +0900

    add iox.CopyFile()

--- iox/file.go
+++ iox/file.go
@@ -2,7 +2,9 @@ package iox
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 // FileExists check if file exists
@@ -15,6 +17,36 @@ func FileExists(path string) error {
 		return fmt.Errorf("%q is directory", path)
 	}
 	return nil
+}
+
+// CopyFile copy src file to des file
+func CopyFile(src string, dst string) error {
+	ss, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !ss.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	dd := filepath.Dir(dst)
+	os.MkdirAll(dd, ss.Mode().Perm())
+
+	sf, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
+
+	df, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE, ss.Mode().Perm())
+	if err != nil {
+		return err
+	}
+	defer df.Close()
+
+	_, err = io.Copy(df, sf)
+	return err
 }
 
 // FileReader a file reader

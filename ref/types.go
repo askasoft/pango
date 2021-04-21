7d12041cac4d1852a432f557f0452df54db7a24c
Frank Wang <yf.frank.wang@outlook.com>
2021-04-21 14:51:52 +0900

    add ref/types.go

---
+++ ref/types.go
@@ -0,0 +1,32 @@
+package ref
+
+// IsIntType return true if v is an integer
+func IsIntType(v interface{}) bool {
+	switch v.(type) {
+	case int, int8, int16, int32, int64:
+	case uint, uint8, uint16, uint32, uint64:
+		return true
+	}
+
+	return false
+}
+
+// IsFloatType return true if v is a float
+func IsFloatType(v interface{}) bool {
+	switch v.(type) {
+	case float32, float64:
+		return true
+	}
+
+	return false
+}
+
+// IsComplexType return true if v is a complex
+func IsComplexType(v interface{}) bool {
+	switch v.(type) {
+	case complex64, complex128:
+		return true
+	}
+
+	return false
+}

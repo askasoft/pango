7558354b489f7e7d5ef4763ca7e57c7d3762c803
Frank Wang <yf.frank.wang@outlook.com>
2021-04-30 09:55:27 +0900

    change Length to LineSize

--- iox/chunker.go
+++ iox/chunker.go
@@ -10,36 +10,36 @@ const PemChunkSize = 64
 
 // ChunkLineWriter limits text to n characters per line
 type ChunkLineWriter struct {
-	Writer io.Writer
-	EOL    string
-	Length int // chunk line size
+	Writer   io.Writer
+	EOL      string
+	LineSize int // chunk line size
 
-	size int // internal writed line size
+	written int // internal written line size
 }
 
 // NewMimeChunkWriter create a writer for split base64 to 76 characters per line
 func NewMimeChunkWriter(w io.Writer) *ChunkLineWriter {
-	return &ChunkLineWriter{Writer: w, EOL: CRLF, Length: MimeChunkSize}
+	return &ChunkLineWriter{Writer: w, EOL: CRLF, LineSize: MimeChunkSize}
 }
 
 // NewPemChunkWriter create a writer for split base64 to 76 characters per line
 func NewPemChunkWriter(w io.Writer) *ChunkLineWriter {
-	return &ChunkLineWriter{Writer: w, EOL: CRLF, Length: PemChunkSize}
+	return &ChunkLineWriter{Writer: w, EOL: CRLF, LineSize: PemChunkSize}
 }
 
 // Write implements io.Writer
-func (w *ChunkLineWriter) Write(p []byte) (n int, err error) {
+func (cw *ChunkLineWriter) Write(p []byte) (n int, err error) {
 	n = 0
-	for len(p)+w.size > w.Length {
-		_, err = w.Writer.Write(p[:w.Length-w.size])
-		_, err = w.Writer.Write([]byte(w.EOL))
-		p = p[w.Length-w.size:]
-		n += w.Length - w.size
-		w.size = 0
+	for len(p)+cw.written > cw.LineSize {
+		_, err = cw.Writer.Write(p[:cw.LineSize-cw.written])
+		_, err = cw.Writer.Write([]byte(cw.EOL))
+		p = p[cw.LineSize-cw.written:]
+		n += cw.LineSize - cw.written
+		cw.written = 0
 	}
 
-	_, err = w.Writer.Write(p)
-	w.size += len(p)
-
-	return n + len(p), err
+	_, err = cw.Writer.Write(p)
+	cw.written += len(p)
+	n += len(p)
+	return
 }

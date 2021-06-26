9a9df714bdfbaffcfc3f36e3d1383d9b79673a6a
Frank Wang <yf.frank.wang@outlook.com>
2021-06-26 17:27:36 +0900

    add httpx.NewHeaderAppender()

--- net/httpx/util.go
+++ net/httpx/util.go
@@ -65,3 +65,29 @@ func GetClientIP(req *http.Request, headers ...string) string {
 	}
 	return ip
 }
+
+// NewHeaderAppender create a http.ResponseWriter to append on WriteHeader(statusCode int).
+// if statusCode != 200, header will not append.
+// a existing header will not be overwriten.
+func NewHeaderAppender(w http.ResponseWriter, h map[string]string) http.ResponseWriter {
+	return &headerWriter{w, h}
+}
+
+// headerWriter write header when statusCode == 200 on WriteHeader(statusCode int)
+// a existing header will not be overwriten.
+type headerWriter struct {
+	http.ResponseWriter
+	header map[string]string
+}
+
+// WriteHeader append header when statusCode == 200
+func (hw *headerWriter) WriteHeader(statusCode int) {
+	if statusCode == http.StatusOK {
+		for k, v := range hw.header {
+			if hw.Header().Get(k) == "" {
+				hw.Header().Add(k, v)
+			}
+		}
+	}
+	hw.ResponseWriter.WriteHeader(statusCode)
+}

ab3e1e37b429cdf7b05f340b3133170c6392003e
Frank Wang <yf.frank.wang@outlook.com>
2021-06-26 21:28:54 +0900

    add httpx.AppendPrefix()/URLReplace() handler

--- net/httpx/fs.go
+++ net/httpx/fs.go
@@ -2,46 +2,9 @@ package httpx
 
 import (
 	"net/http"
-	"net/url"
 	"os"
-	"path/filepath"
 	"time"
 )
-
-// PrefixFS append a prefix to a http.FileSystem.
-func PrefixFS(hfs http.FileSystem, prefix string) http.FileSystem {
-	return prefixFS{hfs, prefix}
-}
-
-type prefixFS struct {
-	hfs    http.FileSystem
-	prefix string
-}
-
-func (pfs prefixFS) Open(name string) (http.File, error) {
-	p := filepath.ToSlash(filepath.Join(pfs.prefix, name))
-	return pfs.hfs.Open(p)
-}
-
-// PrefixHandler returns a handler that serves HTTP requests by appending the
-// given prefix from the request URL's Path (and RawPath if set) and invoking
-// the handler h.
-func PrefixHandler(prefix string, h http.Handler) http.Handler {
-	if prefix == "" {
-		return h
-	}
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		p := prefix + r.URL.Path
-		rp := prefix + r.URL.RawPath
-		r2 := new(http.Request)
-		*r2 = *r
-		r2.URL = new(url.URL)
-		*r2.URL = *r.URL
-		r2.URL.Path = p
-		r2.URL.RawPath = rp
-		h.ServeHTTP(w, r2)
-	})
-}
 
 // FixedModTimeFS returns a FileSystem with fixed ModTime
 func FixedModTimeFS(hfs http.FileSystem, mt time.Time) http.FileSystem {

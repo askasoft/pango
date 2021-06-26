3142a03484fba6de3ea2e11d89d5cf68e725cec1
Frank Wang <yf.frank.wang@outlook.com>
2021-06-26 18:11:39 +0900

    add httpx.StaticFS to fix mod time for embed.FS returns zero ModTime()

--- net/httpx/fs.go
+++ net/httpx/fs.go
@@ -3,7 +3,9 @@ package httpx
 import (
 	"net/http"
 	"net/url"
+	"os"
 	"path/filepath"
+	"time"
 )
 
 // PrefixFS append a prefix to a http.FileSystem.
@@ -39,4 +41,48 @@ func PrefixHandler(prefix string, h http.Handler) http.Handler {
 		r2.URL.RawPath = rp
 		h.ServeHTTP(w, r2)
 	})
+}
+
+// FixedModTimeFS returns a FileSystem with fixed ModTime
+func FixedModTimeFS(hfs http.FileSystem, mt time.Time) http.FileSystem {
+	return &StaticFS{hfs, mt}
+}
+
+// StaticFS a FileSystem with fixed ModTime
+type StaticFS struct {
+	http.FileSystem
+	FixedModTime time.Time
+}
+
+// Open implements http.FileSystem.Open()
+func (sfs *StaticFS) Open(name string) (http.File, error) {
+	file, err := sfs.FileSystem.Open(name)
+	return &StaticFile{File: file, modTime: sfs.FixedModTime}, err
+}
+
+// StaticFile a File with fixed ModTime
+type StaticFile struct {
+	http.File
+	modTime time.Time
+}
+
+// Stat implements File.Stat()
+func (sf *StaticFile) Stat() (os.FileInfo, error) {
+	fi, err := sf.File.Stat()
+	return &StaticFileInfo{FileInfo: fi, modTime: sf.modTime}, err
+}
+
+// StaticFileInfo a FileInfo with fixed ModTime
+type StaticFileInfo struct {
+	os.FileInfo
+	modTime time.Time
+}
+
+// ModTime implements FileInfo.ModTime()
+func (sfi *StaticFileInfo) ModTime() time.Time {
+	mt := sfi.FileInfo.ModTime()
+	if mt.IsZero() {
+		return sfi.modTime
+	}
+	return mt
 }

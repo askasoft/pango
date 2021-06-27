3357f30f846f13b62d8af7c62940a7d51136c56b
Frank Wang <yf.frank.wang@outlook.com>
2021-06-27 15:17:14 +0900

    do not export StaticXX struct

--- net/httpx/fs.go
+++ net/httpx/fs.go
@@ -8,41 +8,41 @@ import (
 
 // FixedModTimeFS returns a FileSystem with fixed ModTime
 func FixedModTimeFS(hfs http.FileSystem, mt time.Time) http.FileSystem {
-	return &StaticFS{hfs, mt}
+	return &staticFS{hfs, mt}
 }
 
-// StaticFS a FileSystem with fixed ModTime
-type StaticFS struct {
+// staticFS a FileSystem with fixed ModTime
+type staticFS struct {
 	http.FileSystem
 	FixedModTime time.Time
 }
 
 // Open implements http.FileSystem.Open()
-func (sfs *StaticFS) Open(name string) (http.File, error) {
+func (sfs *staticFS) Open(name string) (http.File, error) {
 	file, err := sfs.FileSystem.Open(name)
-	return &StaticFile{File: file, modTime: sfs.FixedModTime}, err
+	return &staticFile{File: file, modTime: sfs.FixedModTime}, err
 }
 
-// StaticFile a File with fixed ModTime
-type StaticFile struct {
+// staticFile a File with fixed ModTime
+type staticFile struct {
 	http.File
 	modTime time.Time
 }
 
-// Stat implements File.Stat()
-func (sf *StaticFile) Stat() (os.FileInfo, error) {
+// Stat implements http.File.Stat()
+func (sf *staticFile) Stat() (os.FileInfo, error) {
 	fi, err := sf.File.Stat()
-	return &StaticFileInfo{FileInfo: fi, modTime: sf.modTime}, err
+	return &staticFileInfo{FileInfo: fi, modTime: sf.modTime}, err
 }
 
-// StaticFileInfo a FileInfo with fixed ModTime
-type StaticFileInfo struct {
+// staticFileInfo a FileInfo with fixed ModTime
+type staticFileInfo struct {
 	os.FileInfo
 	modTime time.Time
 }
 
 // ModTime implements FileInfo.ModTime()
-func (sfi *StaticFileInfo) ModTime() time.Time {
+func (sfi *staticFileInfo) ModTime() time.Time {
 	mt := sfi.FileInfo.ModTime()
 	if mt.IsZero() {
 		return sfi.modTime

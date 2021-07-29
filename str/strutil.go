1d13eb38784f3515f1f4a96b8d5c7acab45b7516
Frank Wang <yf.frank.wang@outlook.com>
2021-07-29 10:24:26 +0900

    add str.Compare()

--- str/strutil.go
+++ str/strutil.go
@@ -83,3 +83,9 @@ func RemoveEmptys(ss []string) []string {
 	}
 	return ds
 }
+
+// Compare returns an integer comparing two strings lexicographically.
+// The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
+func Compare(a, b string) int {
+	return bytes.Compare(UnsafeBytes(a), UnsafeBytes(b))
+}

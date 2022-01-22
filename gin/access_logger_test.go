ecf8f1b75b2d9ddc824f84a56b29874184ae94a6
Frank Wang <yf.frank.wang@outlook.com>
2022-01-22 13:37:29 +0800

    gin: folk https://github.com/gin-gonic/gin

---
+++ gin/access_logger_test.go
@@ -0,0 +1,133 @@
+package gin
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/pandafw/pango/str"
+)
+
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func assertContains(t *testing.T, msg string, body string, ss ...string) {
+	for _, s := range ss {
+		if !str.Contains(body, s) {
+			t.Errorf(`%s access log does not contains %q`, msg, s)
+		}
+	}
+}
+
+func TestTextLog(t *testing.T) {
+	router := New()
+
+	buffer := new(bytes.Buffer)
+	writer := io.MultiWriter(buffer, os.Stdout)
+	router.Use(NewAccessLogger(writer, DefaultTextLogFormat).Handler())
+
+	router.Any("/example", func(c *Context) {})
+
+	buffer.Reset()
+	performRequest(router, "GET", "/example?a=100")
+	assertContains(t, "GET /example?a=100", buffer.String(), "200", "GET", "/example", "a=100")
+
+	buffer.Reset()
+	performRequest(router, "POST", "/example")
+	assertContains(t, "POST /example", buffer.String(), "200", "POST", "/example")
+
+	buffer.Reset()
+	performRequest(router, "PUT", "/example")
+	assertContains(t, "PUT /example", buffer.String(), "200", "PUT", "/example")
+
+	buffer.Reset()
+	performRequest(router, "DELETE", "/example")
+	assertContains(t, "DELETE /example", buffer.String(), "200", "DELETE", "/example")
+
+	buffer.Reset()
+	performRequest(router, "PATCH", "/example")
+	assertContains(t, "PATCH /example", buffer.String(), "200", "PATCH", "/example")
+
+	buffer.Reset()
+	performRequest(router, "HEAD", "/example")
+	assertContains(t, "HEAD /example", buffer.String(), "200", "HEAD", "/example")
+
+	buffer.Reset()
+	performRequest(router, "OPTIONS", "/example")
+	assertContains(t, "OPTIONS /example", buffer.String(), "200", "OPTIONS", "/example")
+
+	buffer.Reset()
+	performRequest(router, "GET", "/notfound")
+	assertContains(t, "GET /notfound", buffer.String(), "404", "GET", "/notfound")
+}
+
+func assertJSONResult(t *testing.T, result map[string]interface{}, sc int, method string, url string) {
+	if result["status"] != float64(sc) {
+		t.Errorf("status = %v, want %v", result["status"], sc)
+	}
+	if result["method"] != method {
+		t.Errorf("method = %v, want %v", result["method"], sc)
+	}
+	if result["url"] != url {
+		t.Errorf("url = %v, want %v", result["url"], sc)
+	}
+}
+
+func TestJSONLog(t *testing.T) {
+	result := make(map[string]interface{})
+	buffer := new(bytes.Buffer)
+	router := New()
+
+	writer := io.MultiWriter(buffer, os.Stdout)
+	router.Use(NewAccessLogger(writer, DefaultJSONLogFormat).Handler())
+
+	router.Any("/example", func(c *Context) {})
+
+	buffer.Reset()
+	performRequest(router, "GET", "/example?a=100")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "GET", "/example?a=100")
+
+	buffer.Reset()
+	performRequest(router, "POST", "/example")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "POST", "/example")
+
+	buffer.Reset()
+	performRequest(router, "PUT", "/example")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "PUT", "/example")
+
+	buffer.Reset()
+	performRequest(router, "DELETE", "/example")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "DELETE", "/example")
+
+	buffer.Reset()
+	performRequest(router, "PATCH", "/example")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "PATCH", "/example")
+
+	buffer.Reset()
+	performRequest(router, "HEAD", "/example")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "HEAD", "/example")
+
+	buffer.Reset()
+	performRequest(router, "OPTIONS", "/example")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 200, "OPTIONS", "/example")
+
+	buffer.Reset()
+	performRequest(router, "GET", "/notfound")
+	json.Unmarshal(buffer.Bytes(), &result)
+	assertJSONResult(t, result, 404, "GET", "/notfound")
+}
---
+++ gin/gin_integration_linux_test.go
@@ -0,0 +1,83 @@
+package gin
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnixSocket(t *testing.T) {
+	router := New()
+
+	unixTestSocket := filepath.Join(os.TempDir(), "unix_unit_test")
+
+	defer os.Remove(unixTestSocket)
+
+	go func() {
+		router.GET("/example", func(c *Context) { c.String(http.StatusOK, "it worked") })
+		assert.NoError(t, router.RunUnix(unixTestSocket))
+	}()
+	// have to wait for the goroutine to start and run the server
+	// otherwise the main thread will complete
+	time.Sleep(5 * time.Millisecond)
+
+	c, err := net.Dial("unix", unixTestSocket)
+	assert.NoError(t, err)
+
+	fmt.Fprint(c, "GET /example HTTP/1.0\r\n\r\n")
+	scanner := bufio.NewScanner(c)
+	var response string
+	for scanner.Scan() {
+		response += scanner.Text()
+	}
+	assert.Contains(t, response, "HTTP/1.0 200", "should get a 200")
+	assert.Contains(t, response, "it worked", "resp body should match")
+}
+
+func TestBadUnixSocket(t *testing.T) {
+	router := New()
+	assert.Error(t, router.RunUnix("#/tmp/unix_unit_test"))
+}
+
+func TestFileDescriptor(t *testing.T) {
+	router := New()
+
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	assert.NoError(t, err)
+	listener, err := net.ListenTCP("tcp", addr)
+	assert.NoError(t, err)
+	socketFile, err := listener.File()
+	assert.NoError(t, err)
+
+	go func() {
+		router.GET("/example", func(c *Context) { c.String(http.StatusOK, "it worked") })
+		assert.NoError(t, router.RunFd(int(socketFile.Fd())))
+	}()
+	// have to wait for the goroutine to start and run the server
+	// otherwise the main thread will complete
+	time.Sleep(5 * time.Millisecond)
+
+	c, err := net.Dial("tcp", listener.Addr().String())
+	assert.NoError(t, err)
+
+	fmt.Fprintf(c, "GET /example HTTP/1.0\r\n\r\n")
+	scanner := bufio.NewScanner(c)
+	var response string
+	for scanner.Scan() {
+		response += scanner.Text()
+	}
+	assert.Contains(t, response, "HTTP/1.0 200", "should get a 200")
+	assert.Contains(t, response, "it worked", "resp body should match")
+}
+
+func TestBadFileDescriptor(t *testing.T) {
+	router := New()
+	assert.Error(t, router.RunFd(0))
+}

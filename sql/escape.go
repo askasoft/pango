1adec5f6aeb0504f326bfd89a0d8c641cd6fcdff
Frank Wang <yf.frank.wang@outlook.com>
2022-04-13 22:00:07 +0900

    add sql/escape.go

---
+++ sql/escape.go
@@ -0,0 +1,50 @@
+package sql
+
+import "strings"
+
+// EscapeLike escape sql like string
+func EscapeLike(str string) string {
+	sb := strings.Builder{}
+
+	for _, c := range str {
+		if c == '~' {
+			sb.WriteRune('~')
+			sb.WriteRune('~')
+			continue
+		}
+
+		if c == '%' || c == '_' {
+			sb.WriteRune('~')
+			sb.WriteRune(c)
+			continue
+		}
+
+		sb.WriteRune(c)
+	}
+
+	return sb.String()
+}
+
+// At present, this method only turns single-quotes into doubled single-quotes (
+// <code>"McHale's Navy"</code> => <code>"McHale''s Navy"</code>). It does not handle the cases
+// of percent (%) or underscore (_) for use in LIKE clauses.
+// </p>
+// see http://www.jguru.com/faq/view.jsp?EID=8881
+func EscapeString(str string) string {
+	return strings.ReplaceAll(str, "'", "''")
+}
+
+// StringLike build a string for like '%' + str + '%'
+func StringLike(str string) string {
+	return "%" + EscapeLike(str) + "%"
+}
+
+// StartsLike build a string for like prefix str + '%'
+func StartsLike(str string) string {
+	return EscapeLike(str) + "%"
+}
+
+// StartsLike build a string for like suffix '%' + str
+func EndsLike(str string) string {
+	return "%" + EscapeLike(str)
+}
---
+++ sql/escape_test.go
@@ -0,0 +1,91 @@
+package sql
+
+import (
+	"testing"
+)
+
+func TestEscapeLike(t *testing.T) {
+	cs := []struct {
+		s, w string
+	}{
+		{"ab~c", "ab~~c"},
+		{"ab%", "ab~%"},
+		{"ab_", "ab~_"},
+	}
+
+	for i, c := range cs {
+		a := EscapeLike(c.s)
+		if a != c.w {
+			t.Errorf("[%d] EscapeLike(%q) = %v, want %v", i, c.s, a, c.w)
+		}
+	}
+}
+
+func TestEscapeString(t *testing.T) {
+	cs := []struct {
+		s, w string
+	}{
+		{"ab'c", "ab''c"},
+	}
+
+	for i, c := range cs {
+		a := EscapeString(c.s)
+		if a != c.w {
+			t.Errorf("[%d] EscapeString(%q) = %v, want %v", i, c.s, a, c.w)
+		}
+	}
+}
+
+func TestStringLike(t *testing.T) {
+	cs := []struct {
+		s, w string
+	}{
+		{"abc", "%abc%"},
+		{"ab~c", "%ab~~c%"},
+		{"ab%", "%ab~%%"},
+		{"ab_", "%ab~_%"},
+	}
+
+	for i, c := range cs {
+		a := StringLike(c.s)
+		if a != c.w {
+			t.Errorf("[%d] StringLike(%q) = %v, want %v", i, c.s, a, c.w)
+		}
+	}
+}
+
+func TestStartsLike(t *testing.T) {
+	cs := []struct {
+		s, w string
+	}{
+		{"abc", "abc%"},
+		{"ab~c", "ab~~c%"},
+		{"ab%", "ab~%%"},
+		{"ab_", "ab~_%"},
+	}
+
+	for i, c := range cs {
+		a := StartsLike(c.s)
+		if a != c.w {
+			t.Errorf("[%d] StartsLike(%q) = %v, want %v", i, c.s, a, c.w)
+		}
+	}
+}
+
+func TestEndsLike(t *testing.T) {
+	cs := []struct {
+		s, w string
+	}{
+		{"abc", "%abc"},
+		{"ab~c", "%ab~~c"},
+		{"ab%", "%ab~%"},
+		{"ab_", "%ab~_"},
+	}
+
+	for i, c := range cs {
+		a := EndsLike(c.s)
+		if a != c.w {
+			t.Errorf("[%d] EndsLike(%q) = %v, want %v", i, c.s, a, c.w)
+		}
+	}
+}

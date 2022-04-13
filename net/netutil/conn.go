fe00443655f58cec2306e1ffd18dfe7852b62a8b
Frank Wang <yf.frank.wang@outlook.com>
2022-04-14 08:23:26 +0900

    netutil: export ConnDumper, ConDebugger

--- net/netutil/conn.go
+++ net/netutil/conn.go
@@ -1,32 +1,36 @@
 package netutil
 
 import (
+	"fmt"
 	"io"
 	"net"
+	"time"
 )
 
-// connDumper a connection dump utility
-type connDumper struct {
+// DumpConn wrap a net.conn for dump
+func DumpConn(conn net.Conn, recv io.Writer, send io.Writer) net.Conn {
+	return &ConnDumper{conn, recv, send}
+}
+
+// ConnDumper a connection dump utility
+type ConnDumper struct {
 	net.Conn
 	Recv io.Writer
 	Send io.Writer
 }
 
-// DumpConn wrap a net.conn for dump
-func DumpConn(conn net.Conn, recv io.Writer, send io.Writer) net.Conn {
-	return &connDumper{conn, recv, send}
-}
-
 // Read reads data from the connection.
 // Read can be made to time out and return an error after a fixed
 // time limit; see SetDeadline and SetReadDeadline.
-func (cd *connDumper) Read(b []byte) (int, error) {
+func (cd *ConnDumper) Read(b []byte) (int, error) {
 	n, err := cd.Conn.Read(b)
-	if n > 0 {
-		cd.Recv.Write(b[:n]) //nolint: errcheck
-	}
-	if err != nil {
-		io.WriteString(cd.Recv, err.Error()) //nolint: errcheck
+	if cd.Recv != nil {
+		if n > 0 {
+			cd.Recv.Write(b[:n]) //nolint: errcheck
+		}
+		if err != nil {
+			io.WriteString(cd.Recv, err.Error()) //nolint: errcheck
+		}
 	}
 	return n, err
 }
@@ -34,35 +38,45 @@ func (cd *connDumper) Read(b []byte) (int, error) {
 // Write writes data to the connection.
 // Write can be made to time out and return an error after a fixed
 // time limit; see SetDeadline and SetWriteDeadline.
-func (cd *connDumper) Write(b []byte) (int, error) {
+func (cd *ConnDumper) Write(b []byte) (int, error) {
 	n, err := cd.Conn.Write(b)
-	if n > 0 {
-		cd.Send.Write(b[:n]) //nolint: errcheck
-	}
-	if err != nil {
-		io.WriteString(cd.Send, err.Error()) //nolint: errcheck
+	if cd.Send != nil {
+		if n > 0 {
+			cd.Send.Write(b[:n]) //nolint: errcheck
+		}
+		if err != nil {
+			io.WriteString(cd.Send, err.Error()) //nolint: errcheck
+		}
 	}
 	return n, err
 }
 
 // Close closes the connection.
 // Any blocked Read or Write operations will be unblocked and return errors.
-func (cd *connDumper) Close() error {
-	if c, ok := cd.Recv.(io.Closer); ok {
-		c.Close()
+func (cd *ConnDumper) Close() (err error) {
+	err = cd.Conn.Close()
+	if cd.Recv != nil {
+		if c, ok := cd.Recv.(io.Closer); ok {
+			c.Close()
+		}
 	}
-	if c, ok := cd.Send.(io.Closer); ok {
-		c.Close()
+	if cd.Send != nil {
+		if c, ok := cd.Send.(io.Closer); ok {
+			c.Close()
+		}
 	}
-	return cd.Conn.Close()
+	return
 }
 
 //-----------------------------------------------
-const stateRecv = -1
-const stateSend = 1
+const (
+	debugTimeFormat = "2006-01-02T15:04:05.000"
+	stateRecv       = -1
+	stateSend       = 1
+)
 
-// connDumper1 a connection dump utility
-type connDumper1 struct {
+// ConnDebugger a connection debug utility
+type ConnDebugger struct {
 	net.Conn
 
 	Writer     io.Writer
@@ -70,21 +84,29 @@ type connDumper1 struct {
 	RecvSuffix string
 	SendPrefix string
 	SendSuffix string
+	Timestamp  bool
 
 	state int
+}
+
+func (cd *ConnDebugger) timestamp(s string) string {
+	if cd.Timestamp {
+		s = fmt.Sprintf(s, time.Now().Format(debugTimeFormat))
+	}
+	return s
 }
 
 // Read reads data from the connection.
 // Read can be made to time out and return an error after a fixed
 // time limit; see SetDeadline and SetReadDeadline.
-func (cd *connDumper1) Read(b []byte) (int, error) {
+func (cd *ConnDebugger) Read(b []byte) (int, error) {
 	n, err := cd.Conn.Read(b)
 
 	if cd.state == stateSend {
-		io.WriteString(cd.Writer, cd.SendSuffix) //nolint: errcheck
+		io.WriteString(cd.Writer, cd.timestamp(cd.SendSuffix)) //nolint: errcheck
 	}
 	if cd.state != stateRecv {
-		io.WriteString(cd.Writer, cd.RecvPrefix) //nolint: errcheck
+		io.WriteString(cd.Writer, cd.timestamp(cd.RecvPrefix)) //nolint: errcheck
 	}
 
 	cd.state = stateRecv
@@ -101,14 +123,14 @@ func (cd *connDumper1) Read(b []byte) (int, error) {
 // Write writes data to the connection.
 // Write can be made to time out and return an error after a fixed
 // time limit; see SetDeadline and SetWriteDeadline.
-func (cd *connDumper1) Write(b []byte) (int, error) {
+func (cd *ConnDebugger) Write(b []byte) (int, error) {
 	n, err := cd.Conn.Write(b)
 
 	if cd.state == stateRecv {
-		io.WriteString(cd.Writer, cd.RecvSuffix) //nolint: errcheck
+		io.WriteString(cd.Writer, cd.timestamp(cd.RecvSuffix)) //nolint: errcheck
 	}
 	if cd.state != stateSend {
-		io.WriteString(cd.Writer, cd.SendPrefix) //nolint: errcheck
+		io.WriteString(cd.Writer, cd.timestamp(cd.SendPrefix)) //nolint: errcheck
 	}
 
 	cd.state = stateSend
@@ -125,12 +147,12 @@ func (cd *connDumper1) Write(b []byte) (int, error) {
 
 // Close closes the connection.
 // Any blocked Read or Write operations will be unblocked and return errors.
-func (cd *connDumper1) Close() error {
+func (cd *ConnDebugger) Close() error {
 	switch cd.state {
 	case stateRecv:
-		io.WriteString(cd.Writer, cd.RecvSuffix) //nolint: errcheck
+		io.WriteString(cd.Writer, cd.timestamp(cd.RecvSuffix)) //nolint: errcheck
 	case stateSend:
-		io.WriteString(cd.Writer, cd.SendSuffix) //nolint: errcheck
+		io.WriteString(cd.Writer, cd.timestamp(cd.SendSuffix)) //nolint: errcheck
 	}
 
 	if c, ok := cd.Writer.(io.Closer); ok {
--- net/netutil/listener.go
+++ net/netutil/listener.go
@@ -15,6 +15,7 @@ type ListenerDumper struct {
 	RecvSuffix string
 	SendPrefix string
 	SendSuffix string
+	Timestamp  bool
 
 	disabled bool   // disable the dumper
 	sequence uint32 // accepted conn sequence
@@ -25,10 +26,11 @@ func DumpListener(listener net.Listener, path string) *ListenerDumper {
 	return &ListenerDumper{
 		Listener:   listener,
 		Path:       path,
-		RecvPrefix: "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\r\n",
+		RecvPrefix: "<<<<<<<<%s<<<<<<<<\r\n",
 		RecvSuffix: "\r\n\r\n",
-		SendPrefix: ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\r\n",
+		SendPrefix: ">>>>>>>>%s>>>>>>>>\r\n",
 		SendSuffix: "\r\n\r\n",
+		Timestamp:  true,
 	}
 }
 
@@ -38,34 +40,40 @@ func (ld *ListenerDumper) Disable(disabled bool) {
 }
 
 // Accept waits for and returns the next connection to the listener.
-func (ld *ListenerDumper) Accept() (net.Conn, error) {
-	conn, err := ld.Listener.Accept()
+func (ld *ListenerDumper) Accept() (conn net.Conn, err error) {
+	conn, err = ld.Listener.Accept()
 	if err != nil || ld.disabled {
-		return conn, err
+		return
 	}
 
+	conn = ld.dump(conn)
+	return
+}
+
+func (ld *ListenerDumper) dump(conn net.Conn) net.Conn {
 	ld.sequence++
 
-	err = os.MkdirAll(ld.Path, os.FileMode(0770))
+	err := os.MkdirAll(ld.Path, os.FileMode(0770))
 	if err != nil {
 		// ignore the dump error
-		return conn, nil // nolint: nilerr
+		return conn
 	}
 
 	fn := filepath.Join(ld.Path, fmt.Sprintf("%08d.log", ld.sequence))
 	file, err := os.OpenFile(fn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.FileMode(0660))
 	if err != nil {
 		// ignore the dump error
-		return conn, nil // nolint: nilerr
+		return conn
 	}
 
-	dcon := &connDumper1{
+	dcon := &ConnDebugger{
 		Conn:       conn,
 		Writer:     file,
 		RecvPrefix: ld.RecvPrefix,
 		RecvSuffix: ld.RecvSuffix,
 		SendPrefix: ld.SendPrefix,
 		SendSuffix: ld.SendSuffix,
+		Timestamp:  true,
 	}
-	return dcon, nil
+	return dcon
 }

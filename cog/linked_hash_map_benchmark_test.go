2cf4ddd3b0d9423b623bd01c5487b02f8f5af4f7
Frank Wang <yf.frank.wang@outlook.com>
2022-05-18 15:10:22 +0900

    cog: add LinkedHashMap

---
+++ cog/linked_hash_map_benchmark_test.go
@@ -0,0 +1,126 @@
+//go:build go1.18
+// +build go1.18
+
+package cog
+
+import "testing"
+
+func benchmarkLinkedHashMapGet(b *testing.B, m *LinkedHashMap[int, any], size int) {
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			m.Get(n)
+		}
+	}
+}
+
+func benchmarkLinkedHashMapSet(b *testing.B, m *LinkedHashMap[int, any], size int) {
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			m.Set(n, struct{}{})
+		}
+	}
+}
+
+func benchmarkLinkedHashMapDel(b *testing.B, m *LinkedHashMap[int, any], size int) {
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			m.Delete(n)
+		}
+	}
+}
+
+func BenchmarkLinkedHashMapGet100(b *testing.B) {
+	b.StopTimer()
+	size := 100
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapGet(b, m, size)
+}
+
+func BenchmarkLinkedHashMapGet1000(b *testing.B) {
+	b.StopTimer()
+	size := 1000
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapGet(b, m, size)
+}
+
+func BenchmarkLinkedHashMapGet10000(b *testing.B) {
+	b.StopTimer()
+	size := 10000
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapGet(b, m, size)
+}
+
+func BenchmarkLinkedHashMapPut100(b *testing.B) {
+	b.StopTimer()
+	size := 100
+	m := NewLinkedHashMap[int, any]()
+	b.StartTimer()
+	benchmarkLinkedHashMapSet(b, m, size)
+}
+
+func BenchmarkLinkedHashMapPut1000(b *testing.B) {
+	b.StopTimer()
+	size := 1000
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapSet(b, m, size)
+}
+
+func BenchmarkLinkedHashMapPut10000(b *testing.B) {
+	b.StopTimer()
+	size := 10000
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapSet(b, m, size)
+}
+
+func BenchmarkLinkedHashMapRemove100(b *testing.B) {
+	b.StopTimer()
+	size := 100
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapDel(b, m, size)
+}
+
+func BenchmarkLinkedHashMapRemove1000(b *testing.B) {
+	b.StopTimer()
+	size := 1000
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapDel(b, m, size)
+}
+
+func BenchmarkLinkedHashMapRemove10000(b *testing.B) {
+	b.StopTimer()
+	size := 10000
+	m := NewLinkedHashMap[int, any]()
+	for n := 0; n < size; n++ {
+		m.Set(n, struct{}{})
+	}
+	b.StartTimer()
+	benchmarkLinkedHashMapDel(b, m, size)
+}
---
+++ cog/linked_hash_map_example_test.go
@@ -0,0 +1,134 @@
+//go:build go1.18
+// +build go1.18
+
+package cog
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func ExampleLinkedHashMap() {
+	m := NewLinkedHashMap[int, string]() // empty (keys are of type int)
+	m.Set(2, "b")                        // 2->b
+	m.Set(1, "x")                        // 2->b, 1->x (insertion-order)
+	m.Set(1, "a")                        // 2->b, 1->a (insertion-order)
+	_, _ = m.Get(2)                      // b, true
+	_, _ = m.Get(3)                      // nil, false
+	_ = m.Values()                       // []interface {}{"b", "a"} (insertion-order)
+	_ = m.Keys()                         // []interface {}{2, 1} (insertion-order)
+	m.Delete(1)                          // 2->b
+	m.Clear()                            // empty
+	m.IsEmpty()                          // true
+	m.Len()                              // 0
+}
+
+func ExampleNewLinkedHashMap() {
+	// initialize from a list of key-value pairs
+	lm := NewLinkedHashMap([]P[string, any]{
+		{"country", "United States"},
+		{"countryCode", "US"},
+		{"region", "CA"},
+		{"regionName", "California"},
+		{"city", "Mountain View"},
+		{"zip", "94043"},
+		{"lat", 37.4192},
+		{"lon", -122.0574},
+		{"timezone", "America/Los_Angeles"},
+		{"isp", "Google Cloud"},
+		{"org", "Google Cloud"},
+		{"as", "AS15169 Google Inc."},
+		{"mobile", true},
+		{"proxy", false},
+		{"query", "35.192.xx.xxx"},
+	}...)
+
+	for it := lm.Iterator(); it.Next(); {
+		fmt.Printf("%-12s: %v\n", it.Key(), it.Value())
+	}
+
+	// Output:
+	// country     : United States
+	// countryCode : US
+	// region      : CA
+	// regionName  : California
+	// city        : Mountain View
+	// zip         : 94043
+	// lat         : 37.4192
+	// lon         : -122.0574
+	// timezone    : America/Los_Angeles
+	// isp         : Google Cloud
+	// org         : Google Cloud
+	// as          : AS15169 Google Inc.
+	// mobile      : true
+	// proxy       : false
+	// query       : 35.192.xx.xxx
+}
+
+func ExampleLinkedHashMap_UnmarshalJSON() {
+	const jsonStream = `{
+  "country"     : "United States",
+  "countryCode" : "US",
+  "region"      : "CA",
+  "regionName"  : "California",
+  "city"        : "Mountain View",
+  "zip"         : "94043",
+  "lat"         : "37.4192",
+  "lon"         : "-122.0574",
+  "timezone"    : "America/Los_Angeles",
+  "isp"         : "Google Cloud",
+  "org"         : "Google Cloud",
+  "as"          : "AS15169 Google Inc.",
+  "mobile"      : "true",
+  "proxy"       : "false",
+  "query"       : "35.192.xx.xxx"
+}`
+
+	// compare with if using a regular generic map, the unmarshalled result
+	//  is a map with unpredictable order of keys
+	var m map[string]any
+	err := json.Unmarshal([]byte(jsonStream), &m)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	for key := range m {
+		// fmt.Printf("%-12s: %v\n", key, m[key])
+		_ = key
+	}
+
+	// use the LinkedHashMap to Unmarshal from JSON object
+	lm := NewLinkedHashMap[string, string]()
+	err = json.Unmarshal([]byte(jsonStream), lm)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	// loop over all key-value pairs,
+	// it is ok to call Set append-modify new key-value pairs,
+	// but not safe to call Delete during iteration.
+	for it := lm.Iterator(); it.Next(); {
+		fmt.Printf("%-12s: %v\n", it.Key(), it.Value())
+		if it.Key() == "city" {
+			lm.Set("mobile", "false")
+			lm.Set("extra", "42")
+		}
+	}
+
+	// Output:
+	// country     : United States
+	// countryCode : US
+	// region      : CA
+	// regionName  : California
+	// city        : Mountain View
+	// zip         : 94043
+	// lat         : 37.4192
+	// lon         : -122.0574
+	// timezone    : America/Los_Angeles
+	// isp         : Google Cloud
+	// org         : Google Cloud
+	// as          : AS15169 Google Inc.
+	// mobile      : false
+	// proxy       : false
+	// query       : 35.192.xx.xxx
+	// extra       : 42
+}

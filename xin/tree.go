11014044c7eb1de8cdc45bd012f25ff40e2336cb
Frank Wang <yf.frank.wang@outlook.com>
2022-12-09 09:58:35 +0900

    xin: fix bug for RedirectTrailingSlash not working if there are more than one child route

--- xin/tree.go
+++ xin/tree.go
@@ -104,7 +104,8 @@ func countSections(path string) uint16 {
 type nodeType uint8
 
 const (
-	root nodeType = iota + 1
+	static nodeType = iota
+	root
 	param
 	catchAll
 )
@@ -170,6 +171,7 @@ walk:
 			child := node{
 				path:      n.path[i:],
 				wildChild: n.wildChild,
+				nType:     static,
 				indices:   n.indices,
 				children:  n.children,
 				handlers:  n.handlers,
@@ -597,6 +599,11 @@ walk: // Outer loop for walking the tree
 			// wildcard child, there must be a handle for this path with an
 			// additional trailing slash
 			if path == "/" && n.wildChild && n.nType != root {
+				value.tsr = true
+				return
+			}
+
+			if path == "/" && n.nType == static {
 				value.tsr = true
 				return
 			}

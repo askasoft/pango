016d16dbf61b4c922979c34189610e150182042e
Frank Wang <yf.frank.wang@outlook.com>
2023-07-31 08:04:18 +0900

    num: add AbsInt()

---
+++ num/math.go
@@ -0,0 +1,22 @@
+package num
+
+func AbsInt(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func AbsInt32(n int32) int32 {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func AbsInt64(n int64) int64 {
+	if n < 0 {
+		return -n
+	}
+	return n
+}

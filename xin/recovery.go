ef3fbf0c7881dbda3e31f92e80da7c693a51d9ce
Frank Wang <yf.frank.wang@outlook.com>
2023-07-31 21:11:29 +0900

    xin: add CustomRecovery()

--- xin/recovery.go
+++ xin/recovery.go
@@ -9,6 +9,56 @@ import (
 	"github.com/askasoft/pango/str"
 )
 
+// RecoveryFunc defines the function passable to CustomRecovery.
+type RecoveryFunc func(c *Context, err interface{})
+
+// Recovery returns a middleware that recovers from any panics and writes a 500 if there was one.
+func Recovery() HandlerFunc {
+	return CustomRecovery(defaultRecover)
+}
+
+// CustomRecovery returns a middleware that recovers from any panics and calls the provided handle func to handle it.
+func CustomRecovery(r RecoveryFunc) HandlerFunc {
+	return func(c *Context) {
+		defer Recover(c, r)
+
+		c.Next()
+	}
+}
+
+func defaultRecover(c *Context, err any) {
+	loggerRecover(c, err)
+	c.AbortWithStatus(http.StatusInternalServerError)
+}
+
+func loggerRecover(c *Context, err any) {
+	c.Logger.Errorf("Panic: %v", err)
+}
+
+func doRecovery(c *Context, err any, r RecoveryFunc) {
+	if IsBrokenPipeError(err) {
+		c.Logger.Debugf("Abort: %v", err)
+
+		// connection is dead, we can't write a status to it.
+		c.Abort()
+		return
+	}
+
+	r(c, err)
+}
+
+func Recover(c *Context, r RecoveryFunc) {
+	defer func() {
+		if err := recover(); err != nil {
+			doRecovery(c, err, loggerRecover)
+		}
+	}()
+
+	if err := recover(); err != nil {
+		doRecovery(c, err, r)
+	}
+}
+
 // IsBrokenPipeError Check for a broken connection error
 func IsBrokenPipeError(err any) bool {
 	if err != nil {
@@ -24,27 +74,4 @@ func IsBrokenPipeError(err any) bool {
 		}
 	}
 	return false
-}
-
-// Recovery returns a middleware that recovers from any panics and writes a 500 if there was one.
-func Recovery() HandlerFunc {
-	return func(c *Context) {
-		defer func() {
-			if err := recover(); err != nil {
-				// Check for a broken connection, as it is not really a
-				// condition that warrants a panic stack trace.
-				if IsBrokenPipeError(err) {
-					c.Logger.Debug("Abort: %v", err)
-					// If the connection is dead, we can't write a status to it.
-					c.Abort()
-					return
-				}
-
-				c.Logger.Errorf("Panic: %v", err)
-				c.AbortWithStatus(http.StatusInternalServerError)
-			}
-		}()
-
-		c.Next()
-	}
 }
---
+++ xin/recovery_test.go
@@ -0,0 +1,93 @@
+package xin
+
+import (
+	"net"
+	"net/http"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestPanicWithAbort assert that panic has been recovered even if context.Abort was used.
+func TestPanicWithAbort(t *testing.T) {
+	e := "Oupps, Houston, we have a problem"
+
+	router := New()
+	router.Use(Recovery())
+	router.GET("/recovery", func(c *Context) {
+		c.AbortWithStatus(http.StatusBadRequest)
+		panic(e)
+	})
+
+	w := PerformRequest(router, "GET", "/recovery")
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, "", w.Body.String())
+}
+
+// TestPanicWithBrokenPipe asserts that recovery specifically handles
+// writing responses to broken pipes
+func TestPanicWithBrokenPipe(t *testing.T) {
+	const expectCode = 204
+
+	expectMsgs := map[syscall.Errno]string{
+		syscall.EPIPE:      "broken pipe",
+		syscall.ECONNRESET: "connection reset by peer",
+	}
+
+	for errno, expectMsg := range expectMsgs {
+		t.Run(expectMsg, func(t *testing.T) {
+			router := New()
+			router.Use(Recovery())
+			router.GET("/recovery", func(c *Context) {
+				c.String(expectCode, expectMsg)
+
+				// Oops. Client connection closed
+				e := &net.OpError{Err: &os.SyscallError{Err: errno}}
+				panic(e)
+			})
+
+			w := PerformRequest(router, "GET", "/recovery")
+			assert.Equal(t, expectCode, w.Code)
+			//assert.Equal(t, expectMsg, w.Body.String())
+		})
+	}
+}
+
+func TestPanicCustomRecovery(t *testing.T) {
+	e := "Oupps, Houston, we have a problem"
+
+	router := New()
+	recovery := func(c *Context, err interface{}) {
+		c.String(http.StatusBadRequest, err.(string))
+	}
+	router.Use(CustomRecovery(recovery))
+	router.GET("/recovery", func(c *Context) {
+		c.Status(http.StatusNoContent)
+		panic(e)
+	})
+
+	w := PerformRequest(router, "GET", "/recovery")
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, e, w.Body.String())
+}
+
+func TestPanicInCustomRecovery(t *testing.T) {
+	e := "Oupps, Houston, we have a problem. "
+
+	router := New()
+	recovery := func(c *Context, err interface{}) {
+		c.String(http.StatusBadRequest, err.(string))
+		panic(e + e)
+	}
+	router.Use(CustomRecovery(recovery))
+	router.GET("/recovery", func(c *Context) {
+		c.Status(http.StatusNoContent)
+		panic(e)
+	})
+
+	w := PerformRequest(router, "GET", "/recovery")
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, e, w.Body.String())
+}

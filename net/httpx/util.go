5e55be4430392496c04dae88c25e7bc05e71bb69
Frank Wang <yf.frank.wang@outlook.com>
2024-01-01 12:35:09 +0900

    httpx: change NewHeaderAppender() to NewHeaderWriter(), NewHeaadersWriter()

--- net/httpx/util.go
+++ net/httpx/util.go
@@ -7,22 +7,47 @@ import (
 	"github.com/askasoft/pango/str"
 )
 
-// NewHeaderAppender create a http.ResponseWriter to append on WriteHeader(statusCode int).
+// NewHeaderWriter create a http.ResponseWriter to append on WriteHeader(statusCode int).
 // if statusCode != 200, header will not append.
 // a existing header will not be overwriten.
-func NewHeaderAppender(w http.ResponseWriter, h map[string]string) http.ResponseWriter {
-	return &headerWriter{w, h}
+func NewHeaderWriter(w http.ResponseWriter, key, value string) http.ResponseWriter {
+	return &headerWriter{w, key, value}
 }
 
 // headerWriter write header when statusCode == 200 on WriteHeader(statusCode int)
 // a existing header will not be overwriten.
 type headerWriter struct {
 	http.ResponseWriter
-	header map[string]string
+	key   string
+	value string
 }
 
 // WriteHeader append header when statusCode == 200
 func (hw *headerWriter) WriteHeader(statusCode int) {
+	if statusCode == http.StatusOK {
+		if hw.Header().Get(hw.key) == "" {
+			hw.Header().Add(hw.key, hw.value)
+		}
+	}
+	hw.ResponseWriter.WriteHeader(statusCode)
+}
+
+// NewHeadersWriter create a http.ResponseWriter to append on WriteHeader(statusCode int).
+// if statusCode != 200, header will not append.
+// a existing header will not be overwriten.
+func NewHeadersWriter(w http.ResponseWriter, h map[string]string) http.ResponseWriter {
+	return &headersWriter{w, h}
+}
+
+// headersWriter write header when statusCode == 200 on WriteHeader(statusCode int)
+// a existing header will not be overwriten.
+type headersWriter struct {
+	http.ResponseWriter
+	header map[string]string
+}
+
+// WriteHeader append header when statusCode == 200
+func (hw *headersWriter) WriteHeader(statusCode int) {
 	if statusCode == http.StatusOK {
 		for k, v := range hw.header {
 			if hw.Header().Get(k) == "" {

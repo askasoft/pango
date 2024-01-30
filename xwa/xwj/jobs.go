7869b9642e42c708bb18c3bdb67a2440b4cba670
Frank Wang <yf.frank.wang@outlook.com>
2024-01-30 12:19:36 +0900

    xwj: add CleanOutdatedJobs()

--- xwa/xwj/jobs.go
+++ xwa/xwj/jobs.go
@@ -20,147 +20,6 @@ var (
 	mutex    sync.Mutex
 )
 
-func RependingJobs(db *gorm.DB, delay time.Duration) {
-	ut := time.Now().Add(-delay)
-	tx := db.Where("status = ? AND updated_at < ?", xwm.JobStatusRunning, ut)
-
-	job := &xwm.Job{RID: 0, Status: xwm.JobStatusPending, Error: ""}
-	r := tx.Select("rid", "status", "error").Updates(job)
-	if r.Error != nil {
-		log.Errorf("Failed to RependingJobs(): %v", r.Error)
-		return
-	}
-	if r.RowsAffected > 0 {
-		log.Infof("RependingJobs: %d", r.RowsAffected)
-	}
-}
-
-func PendingJob(db *gorm.DB, name string, param string) (int64, error) {
-	job := &xwm.Job{Name: name, Param: param, Status: xwm.JobStatusPending}
-	r := db.Create(job)
-	if r.Error != nil {
-		log.Errorf("Failed to pending job %s: %v", job.String(), r.Error)
-	}
-	return job.ID, r.Error
-}
-
-func GetJob(db *gorm.DB, jid int64, details ...bool) (*xwm.Job, error) {
-	job := &xwm.Job{}
-
-	tx := db.Model(job)
-	if len(details) <= 0 || !details[0] {
-		tx = tx.Select("id", "rid", "name", "status")
-	}
-
-	r := tx.Take(job, jid)
-	if errors.Is(r.Error, gorm.ErrRecordNotFound) {
-		return nil, nil
-	}
-	if r.Error != nil {
-		log.Errorf("Failed to get job #%d: %v", jid, r.Error)
-		return nil, r.Error
-	}
-	return job, nil
-}
-
-// GetJobLogs get job logs
-// set levels to ("I", "W", "E", "F") to filter DEBUG/TRACE logs
-func GetJobLogs(db *gorm.DB, jid int64, start, limit int, levels ...string) ([]*xwm.JobLog, error) {
-	var jls []*xwm.JobLog
-
-	tx := db.Where("jid = ?", jid)
-	if len(levels) > 0 {
-		tx.Where("level IN ?", levels)
-	}
-
-	r := tx.Order("id asc").Offset(start).Limit(limit).Find(&jls)
-	if r.Error != nil {
-		log.Errorf("Failed to get job logs #%d: %v", jid, r.Error)
-	}
-	return jls, r.Error
-}
-
-func FindJob(db *gorm.DB, name string, details ...bool) (*xwm.Job, error) {
-	job := &xwm.Job{}
-
-	tx := db.Model(job).Where("name = ?", name).Order("id desc")
-	if len(details) <= 0 || !details[0] {
-		tx = tx.Select("id", "rid", "name", "status")
-	}
-
-	r := tx.First(job)
-	if errors.Is(r.Error, gorm.ErrRecordNotFound) {
-		return nil, nil
-	}
-	if r.Error != nil {
-		log.Errorf("Failed to find job %q: %v", name, r.Error)
-	}
-	return job, r.Error
-}
-
-func FindAndAbortJob(db *gorm.DB, name, reason string) error {
-	job, err := FindJob(db, name)
-	if err != nil {
-		return err
-	}
-
-	return AbortJob(db, job.ID, reason)
-}
-
-func AbortJob(db *gorm.DB, jid int64, reason string) error {
-	job := &xwm.Job{Status: xwm.JobStatusAborted, Error: reason}
-	tx := db.Where("id = ? AND status IN ?", jid, []string{xwm.JobStatusPending, xwm.JobStatusRunning})
-	r := tx.Select("status", "error").Updates(job)
-	if r.Error != nil {
-		return r.Error
-	}
-	if r.RowsAffected != 1 {
-		return ErrJobMissing
-	}
-	return nil
-}
-
-func StartJobs(db *gorm.DB, limit int, run func(*xwm.Job)) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	current := atomic.LoadInt32(&runnings)
-
-	log.Debugf("Current running jobs: %d / %d", current, limit)
-
-	remain := limit - int(current)
-	if remain <= 0 {
-		return
-	}
-
-	var jobs []*xwm.Job
-
-	r := db.Where("status = ?", xwm.JobStatusPending).Order("id asc").Limit(remain).Find(&jobs)
-	if r.Error != nil {
-		log.Errorf("Failed to find pending jobs: %v", r.Error)
-		return
-	}
-
-	for _, job := range jobs {
-		go SafeRunJob(job, run)
-	}
-}
-
-func SafeRunJob(job *xwm.Job, run func(*xwm.Job)) {
-	n := atomic.AddInt32(&runnings, 1)
-
-	defer func() {
-		if err := recover(); err != nil {
-			log.Errorf("Panic: %v", err)
-		}
-		atomic.AddInt32(&runnings, -1)
-	}()
-
-	log.Debugf("Start job #%d: #%d %s", n, job.ID, job.Name)
-
-	run(job)
-}
-
 func LocalRunningJobs() int {
 	return int(atomic.LoadInt32(&runnings))
 }
@@ -202,4 +61,167 @@ func Decode(p string, v any) error {
 	}
 
 	return json.Unmarshal(str.UnsafeBytes(p), v)
+}
+
+func RependingJobs(db *gorm.DB, delay time.Duration) (int, error) {
+	ut := time.Now().Add(-delay)
+	tx := db.Where("status = ? AND updated_at < ?", xwm.JobStatusRunning, ut)
+
+	job := &xwm.Job{RID: 0, Status: xwm.JobStatusPending, Error: ""}
+	r := tx.Select("rid", "status", "error").Updates(job)
+	if r.Error != nil {
+		return 0, r.Error
+	}
+	return int(r.RowsAffected), nil
+}
+
+func PendingJob(db *gorm.DB, name string, param string) (int64, error) {
+	job := &xwm.Job{Name: name, Param: param, Status: xwm.JobStatusPending}
+	r := db.Create(job)
+	return job.ID, r.Error
+}
+
+func GetJob(db *gorm.DB, jid int64, details ...bool) (*xwm.Job, error) {
+	job := &xwm.Job{}
+
+	tx := db.Model(job)
+	if len(details) <= 0 || !details[0] {
+		tx = tx.Select("id", "rid", "name", "status")
+	}
+
+	r := tx.Take(job, jid)
+	if errors.Is(r.Error, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	if r.Error != nil {
+		return nil, r.Error
+	}
+	return job, nil
+}
+
+// GetJobLogs get job logs
+// set levels to ("I", "W", "E", "F") to filter DEBUG/TRACE logs
+func GetJobLogs(db *gorm.DB, jid int64, start, limit int, levels ...string) ([]*xwm.JobLog, error) {
+	var jls []*xwm.JobLog
+
+	tx := db.Where("jid = ?", jid)
+	if len(levels) > 0 {
+		tx.Where("level IN ?", levels)
+	}
+
+	r := tx.Order("id asc").Offset(start).Limit(limit).Find(&jls)
+	return jls, r.Error
+}
+
+func FindJob(db *gorm.DB, name string, details ...bool) (*xwm.Job, error) {
+	job := &xwm.Job{}
+
+	tx := db.Model(job).Where("name = ?", name).Order("id desc")
+	if len(details) <= 0 || !details[0] {
+		tx = tx.Select("id", "rid", "name", "status")
+	}
+
+	r := tx.First(job)
+	if errors.Is(r.Error, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	return job, r.Error
+}
+
+func FindAndAbortJob(db *gorm.DB, name, reason string) error {
+	job, err := FindJob(db, name)
+	if err != nil {
+		return err
+	}
+
+	return AbortJob(db, job.ID, reason)
+}
+
+func AbortJob(db *gorm.DB, jid int64, reason string) error {
+	job := &xwm.Job{Status: xwm.JobStatusAborted, Error: reason}
+	tx := db.Where("id = ? AND status IN ?", jid, []string{xwm.JobStatusPending, xwm.JobStatusRunning})
+	r := tx.Select("status", "error").Updates(job)
+	if r.Error != nil {
+		return r.Error
+	}
+	if r.RowsAffected != 1 {
+		return ErrJobMissing
+	}
+	return nil
+}
+
+func StartJobs(db *gorm.DB, limit int, run func(*xwm.Job), loggers ...log.Logger) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	current := atomic.LoadInt32(&runnings)
+
+	logger := getLogger(loggers...)
+	logger.Debugf("Current running jobs: %d / %d", current, limit)
+
+	remain := limit - int(current)
+	if remain <= 0 {
+		return nil
+	}
+
+	var jobs []*xwm.Job
+
+	r := db.Where("status = ?", xwm.JobStatusPending).Order("id asc").Limit(remain).Find(&jobs)
+	if r.Error != nil {
+		return r.Error
+	}
+
+	for _, job := range jobs {
+		go SafeRunJob(job, run, logger)
+	}
+
+	return nil
+}
+
+func SafeRunJob(job *xwm.Job, run func(*xwm.Job), logger log.Logger) {
+	n := atomic.AddInt32(&runnings, 1)
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Errorf("Panic: %v", err)
+		}
+		atomic.AddInt32(&runnings, -1)
+	}()
+
+	logger.Debugf("Start job #%d: #%d %s", n, job.ID, job.Name)
+
+	run(job)
+}
+
+func CleanOutdatedJobs(db *gorm.DB, due time.Time, loggers ...log.Logger) error {
+	logger := getLogger(loggers...)
+
+	ss := []string{xwm.JobStatusAborted, xwm.JobStatusCompleted}
+	r := db.Where("jid IN (SELECT id FROM jobs WHERE status IN ? AND updated_at < ?)", ss, due).Delete(&xwm.JobLog{})
+	if r.Error != nil {
+		logger.Errorf("Failed to delete outdated job logs: %v", r.Error)
+		return r.Error
+	}
+	if r.RowsAffected > 0 {
+		logger.Infof("Delete outdated job logs: %d", r.RowsAffected)
+	}
+
+	r = db.Where("status IN ? AND updated_at < ?", ss, due).Delete(&xwm.Job{})
+	if r.Error != nil {
+		logger.Errorf("Failed to delete outdated jobs: %v", r.Error)
+		return r.Error
+	}
+	if r.RowsAffected > 0 {
+		logger.Infof("Delete outdated jobs: %d", r.RowsAffected)
+	}
+
+	return nil
+}
+
+func getLogger(loggers ...log.Logger) log.Logger {
+	if len(loggers) > 0 {
+		return loggers[0]
+	}
+
+	return log.GetLogger("XWJ")
 }

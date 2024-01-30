27bbb370208f0a066f5041ff75f6f03d8aeaf21a
Frank Wang <yf.frank.wang@outlook.com>
2024-01-31 00:22:59 +0900

    xwm/job: move field status under name

--- xwa/xwm/job.go
+++ xwa/xwm/job.go
@@ -15,9 +15,9 @@ type Job struct {
 	ID        int64     `gorm:"not null;primaryKey;autoIncrement" uri:"id" form:"id" json:"id,omitempty"`
 	RID       int64     `gorm:"column:rid;not null" form:"rid" json:"rid,omitempty"` // job runner id
 	Name      string    `gorm:"size:250;not null;index" json:"name,omitempty"`
+	Status    string    `gorm:"size:1;not null" form:"status" json:"status,omitempty"`
 	Param     string    `gorm:"not null" json:"param,omitempty"`
 	Result    string    `gorm:"not null" json:"result,omitempty"`
-	Status    string    `gorm:"size:1;not null" form:"status" json:"status,omitempty"`
 	Error     string    `gorm:"not null" json:"error,omitempty"`
 	CreatedAt time.Time `gorm:"<-:create;not null" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"not null" json:"updated_at,omitempty"`

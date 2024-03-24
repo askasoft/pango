391f6d60e7714d0db41d0e7fb8923e5bcbf64806
Frank Wang <yf.frank.wang@outlook.com>
2024-03-24 21:48:51 +0900

    xmw.OriginAccessController: add AllowCredentials, AllowMethods, AllowHeaders, ExposeHeaders, MaxAge option to set response header

--- xmw/origin_access_controller.go
+++ xmw/origin_access_controller.go
@@ -4,21 +4,32 @@ import (
 	"net/http"
 
 	"github.com/askasoft/pango/cog"
+	"github.com/askasoft/pango/num"
 	"github.com/askasoft/pango/xin"
 )
 
 // OriginAccessController Access-Control-Allow-Origin response header middleware
-// see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+// see
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
 // if Origins contains the request header 'origin', set the Access-Control-Allow-Origin response header.
 // if the request method is OPTIONS, also set the status code to 200.
 type OriginAccessController struct {
-	Origins *cog.HashSet[string]
-	Headers string
+	AllowOrigins     *cog.HashSet[string]
+	AllowCredentials bool
+	AllowMethods     string
+	AllowHeaders     string
+	ExposeHeaders    string
+	MaxAge           int
 }
 
 // NewOriginAccessController create a default OriginAccessController
 func NewOriginAccessController(origins ...string) *OriginAccessController {
-	return &OriginAccessController{Origins: cog.NewHashSet(origins...)}
+	return &OriginAccessController{AllowOrigins: cog.NewHashSet(origins...)}
 }
 
 // Handler returns the xin.HandlerFunc
@@ -26,19 +37,39 @@ func (ll *OriginAccessController) Handler() xin.HandlerFunc {
 	return ll.Handle
 }
 
-// SetAllowOrigins set allowed origins
+// SetAllowOrigins set allow origins
 func (ll *OriginAccessController) SetAllowOrigins(origins ...string) {
-	ll.Origins = cog.NewHashSet(origins...)
+	ll.AllowOrigins = cog.NewHashSet(origins...)
 }
 
-// SetAllowHeaders set allowed headers
+// SetAllowMethods set Access-Control-Allow-Methods
+func (ll *OriginAccessController) SetAllowMethods(methods string) {
+	ll.AllowMethods = methods
+}
+
+// SetAllowCredentials set allow Credentials
+func (ll *OriginAccessController) SetAllowCredentials(allow bool) {
+	ll.AllowCredentials = allow
+}
+
+// SetAllowHeaders set allow headers
 func (ll *OriginAccessController) SetAllowHeaders(headers string) {
-	ll.Headers = headers
+	ll.AllowHeaders = headers
+}
+
+// SetExposeHeaders set expose headers
+func (ll *OriginAccessController) SetExposeHeaders(headers string) {
+	ll.ExposeHeaders = headers
+}
+
+// SetMaxAge set Access-Control-Max-Age
+func (ll *OriginAccessController) SetMaxAge(sec int) {
+	ll.MaxAge = sec
 }
 
 // Handle process xin request
 func (ll *OriginAccessController) Handle(c *xin.Context) {
-	acaos := ll.Origins
+	acaos := ll.AllowOrigins
 	if acaos.Len() > 0 {
 		acao := ""
 
@@ -53,10 +84,32 @@ func (ll *OriginAccessController) Handle(c *xin.Context) {
 
 		if acao != "" {
 			c.Header("Access-Control-Allow-Origin", acao)
-			acahs := ll.Headers
+
+			acac := ll.AllowCredentials
+			if acac {
+				c.Header("Access-Control-Allow-Credentials", "true")
+			}
+
+			acams := ll.AllowMethods
+			if acams != "" {
+				c.Header("Access-Control-Allow-Methods", acams)
+			}
+
+			acahs := ll.AllowHeaders
 			if acahs != "" {
 				c.Header("Access-Control-Allow-Headers", acahs)
 			}
+
+			acehs := ll.ExposeHeaders
+			if acehs != "" {
+				c.Header("Access-Control-Expose-Headers", acehs)
+			}
+
+			acma := ll.MaxAge
+			if acma > 0 {
+				c.Header("Access-Control-Max-Age", num.Itoa(acma))
+			}
+
 			if c.Request.Method == http.MethodOptions {
 				c.Status(http.StatusOK)
 			}

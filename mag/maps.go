3ca8e013b97cfb0c9c84211f2de76b215b1a476d
Frank Wang <yf.frank.wang@outlook.com>
2024-03-28 00:52:15 +0900

    mag: add Copy() method

--- mag/maps.go
+++ mag/maps.go
@@ -3,6 +3,7 @@
 
 package mag
 
+// Equal check equal for two map a and b.
 func Equal[K comparable, V comparable](a map[K]V, b map[K]V) bool {
 	if len(a) != len(b) {
 		return false
@@ -17,6 +18,14 @@ func Equal[K comparable, V comparable](a map[K]V, b map[K]V) bool {
 	return true
 }
 
+// Copy copy src map to dst map.
+func Copy[K comparable, V any](dst, src map[K]V) {
+	for k, v := range src {
+		dst[k] = v
+	}
+}
+
+// Merge merge all map `ms`, return the merged map.
 func Merge[K comparable, V any](ms ...map[K]V) map[K]V {
 	sz := 0
 	for _, m := range ms {
@@ -32,6 +41,7 @@ func Merge[K comparable, V any](ms ...map[K]V) map[K]V {
 	return mm
 }
 
+// Reverse reverse key/value from map m, return the reversed map
 func Reverse[K comparable, V comparable](m map[K]V) map[V]K {
 	rm := make(map[V]K, len(m))
 	for k, v := range m {

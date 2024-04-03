0501cbedb905ef5376c206781c0092c54d5caed5
Frank Wang <yf.frank.wang@outlook.com>
2024-04-03 15:11:20 +0900

    sqx: fix compileNamedQuery for unique code parameter

--- sqx/named.go
+++ sqx/named.go
@@ -19,9 +19,12 @@ import (
 	"reflect"
 	"regexp"
 	"strconv"
+	"strings"
 	"unicode"
+	"unicode/utf8"
 
 	"github.com/askasoft/pango/ref"
+	"github.com/askasoft/pango/str"
 )
 
 // NamedStmt is a prepared statement that executes named queries.  Prepare it
@@ -131,7 +134,7 @@ type namedPreparer interface {
 
 func prepareNamed(p namedPreparer, query string) (*NamedStmt, error) {
 	bindType := BindType(p.DriverName())
-	q, args, err := compileNamedQuery([]byte(query), bindType)
+	q, args, err := compileNamedQuery(query, bindType)
 	if err != nil {
 		return nil, err
 	}
@@ -211,7 +214,7 @@ func bindMapArgs(names []string, arg map[string]any) ([]any, error) {
 // The rules for binding field names to parameter names follow the same
 // conventions as for StructScan, including obeying the `db` struct tags.
 func bindStruct(bindType int, query string, arg any, m *ref.Mapper) (string, []any, error) {
-	bound, names, err := compileNamedQuery([]byte(query), bindType)
+	bound, names, err := compileNamedQuery(query, bindType)
 	if err != nil {
 		return "", []any{}, err
 	}
@@ -260,7 +263,7 @@ func fixBound(bound string, loop int) string {
 	var buffer bytes.Buffer
 
 	buffer.WriteString(bound[0:closingBracketIndex])
-	for i := 0; i < loop-1; i++ {
+	for i := 1; i < loop; i++ {
 		buffer.WriteString(",")
 		buffer.WriteString(bound[openingBracketIndex:closingBracketIndex])
 	}
@@ -273,26 +276,29 @@ func fixBound(bound string, loop int) string {
 func bindArray(bindType int, query string, arg any, m *ref.Mapper) (string, []any, error) {
 	// do the initial binding with QUESTION;  if bindType is not question,
 	// we can rebind it at the end.
-	bound, names, err := compileNamedQuery([]byte(query), QUESTION)
+	bound, names, err := compileNamedQuery(query, QUESTION)
 	if err != nil {
-		return "", []any{}, err
+		return "", nil, err
 	}
+
 	arrayValue := reflect.ValueOf(arg)
 	arrayLen := arrayValue.Len()
 	if arrayLen == 0 {
-		return "", []any{}, fmt.Errorf("length of array is 0: %#v", arg)
+		return "", nil, fmt.Errorf("length of array is 0: %#v", arg)
 	}
+
 	var arglist = make([]any, 0, len(names)*arrayLen)
 	for i := 0; i < arrayLen; i++ {
 		elemArglist, err := bindAnyArgs(names, arrayValue.Index(i).Interface(), m)
 		if err != nil {
-			return "", []any{}, err
+			return "", nil, err
 		}
 		arglist = append(arglist, elemArglist...)
 	}
 	if arrayLen > 1 {
 		bound = fixBound(bound, arrayLen)
 	}
+
 	// adjust binding type if we weren't on question
 	if bindType != QUESTION {
 		bound = Rebind(bindType, bound)
@@ -302,7 +308,7 @@ func bindArray(bindType int, query string, arg any, m *ref.Mapper) (string, []an
 
 // bindMap binds a named parameter query with a map of arguments.
 func bindMap(bindType int, query string, args map[string]any) (string, []any, error) {
-	bound, names, err := compileNamedQuery([]byte(query), bindType)
+	bound, names, err := compileNamedQuery(query, bindType)
 	if err != nil {
 		return "", []any{}, err
 	}
@@ -319,90 +325,97 @@ func bindMap(bindType int, query string, args map[string]any) (string, []any, er
 // digits and numbers, where '5' is a digit but '五' is not.
 var allowedBindRunes = []*unicode.RangeTable{unicode.Letter, unicode.Digit}
 
-// FIXME: this function isn't safe for unicode named params, as a failing test
-// can testify.  This is not a regression but a failure of the original code
-// as well.  It should be modified to range over runes in a string rather than
-// bytes, even though this is less convenient and slower.  Hopefully the
-// addition of the prepared NamedStmt (which will only do this once) will make
-// up for the slightly slower ad-hoc NamedExec/NamedQuery.
-
-// compile a NamedQuery into an unbound query (using the '?' bindvar) and
-// a list of names.
-func compileNamedQuery(qs []byte, bindType int) (query string, names []string, err error) {
-	names = make([]string, 0, 10)
-	rebound := make([]byte, 0, len(qs))
+// compile a NamedQuery into an unbound query (using the '?' bindvar) and a list of names.
+func compileNamedQuery(qs string, bindType int) (query string, names []string, err error) {
+	names = make([]string, 0, str.CountByte(qs, ':'))
+	rebound := &strings.Builder{}
+	rebound.Grow(len(qs))
 
 	inName := false
-	last := len(qs) - 1
-	currentVar := 1
-	name := make([]byte, 0, 10)
+	vars := 0
+
+	_, n := utf8.DecodeLastRuneInString(qs)
+	last := len(qs) - n
+
+	name := &strings.Builder{}
+	name.Grow(16)
 
 	for i, b := range qs {
 		// a ':' while we're in a name is an error
 		if b == ':' {
-			// if this is the second ':' in a '::' escape sequence, append a ':'
-			if inName && i > 0 && qs[i-1] == ':' {
-				rebound = append(rebound, ':')
-				inName = false
-				continue
-			} else if inName {
+			if inName {
+				// if this is the second ':' in a '::' escape sequence, append a ':'
+				if i > 0 && qs[i-1] == ':' {
+					rebound.WriteByte(':')
+					inName = false
+					continue
+				}
+
 				err = errors.New("unexpected `:` while reading named param at " + strconv.Itoa(i))
 				return query, names, err
 			}
+
+			name.Reset()
 			inName = true
-			name = []byte{}
-		} else if inName && i > 0 && b == '=' && len(name) == 0 {
-			rebound = append(rebound, ':', '=')
-			inName = false
 			continue
+		}
+
+		if inName {
+			if i > 0 && b == '=' && name.Len() == 0 {
+				rebound.WriteString(":=")
+				inName = false
+				continue
+			}
+
 			// if we're in a name, and this is an allowed character, continue
-		} else if inName && (unicode.IsOneOf(allowedBindRunes, rune(b)) || b == '_' || b == '.') && i != last {
-			// append the byte to the name if we are in a name and not on the last byte
-			name = append(name, b)
+			if (unicode.IsOneOf(allowedBindRunes, b) || b == '_' || b == '.') && i != last {
+				// append the byte to the name if we are in a name and not on the last byte
+				name.WriteRune(b)
+				continue
+			}
+
 			// if we're in a name and it's not an allowed character, the name is done
-		} else if inName {
 			inName = false
 			// if this is the final byte of the string and it is part of the name, then
 			// make sure to add it to the name
-			if i == last && unicode.IsOneOf(allowedBindRunes, rune(b)) {
-				name = append(name, b)
+			if i == last && unicode.IsOneOf(allowedBindRunes, b) {
+				name.WriteRune(b)
 			}
+
 			// add the string representation to the names list
-			names = append(names, string(name))
+			names = append(names, name.String())
 			// add a proper bindvar for the bindType
 			switch bindType {
 			// oracle only supports named type bind vars even for positional
 			case NAMED:
-				rebound = append(rebound, ':')
-				rebound = append(rebound, name...)
+				rebound.WriteByte(':')
+				rebound.WriteString(name.String())
 			case QUESTION, UNKNOWN:
-				rebound = append(rebound, '?')
+				rebound.WriteByte('?')
 			case DOLLAR:
-				rebound = append(rebound, '$')
-				for _, b := range strconv.Itoa(currentVar) {
-					rebound = append(rebound, byte(b))
-				}
-				currentVar++
+				vars++
+				rebound.WriteByte('$')
+				rebound.WriteString(strconv.Itoa(vars))
 			case AT:
-				rebound = append(rebound, '@', 'p')
-				for _, b := range strconv.Itoa(currentVar) {
-					rebound = append(rebound, byte(b))
-				}
-				currentVar++
+				vars++
+				rebound.WriteString("@p")
+				rebound.WriteString(strconv.Itoa(vars))
 			}
+
 			// add this byte to string unless it was not part of the name
 			if i != last {
-				rebound = append(rebound, b)
-			} else if !unicode.IsOneOf(allowedBindRunes, rune(b)) {
-				rebound = append(rebound, b)
+				rebound.WriteRune(b)
+			} else if !unicode.IsOneOf(allowedBindRunes, b) {
+				rebound.WriteRune(b)
 			}
-		} else {
-			// this is a normal byte and should just go onto the rebound query
-			rebound = append(rebound, b)
+			continue
 		}
+
+		// this is a normal byte and should just go onto the rebound query
+		rebound.WriteRune(b)
 	}
 
-	return string(rebound), names, err
+	return rebound.String(), names, err
 }
 
 // BindNamed binds a struct or a map to a query with named parameters.

59d573cf4c20f5e99b86f4db6512b0fc1f05f994
Frank Wang <yf.frank.wang@outlook.com>
2024-06-29 10:49:18 +0900

    xvw: add icon template function

--- xvw/funcs.go
+++ xvw/funcs.go
@@ -1,6 +1,9 @@
 package xvw
 
 import (
+	"html/template"
+
+	"github.com/askasoft/pango/str"
 	"github.com/askasoft/pango/tbs"
 	"github.com/askasoft/pango/tpl"
 	"github.com/askasoft/pango/xvw/tags"
@@ -9,6 +12,9 @@ import (
 // Functions default utility functions for template
 func Functions() tpl.FuncMap {
 	return tpl.FuncMap{
+		// Icon
+		"I": icon,
+
 		// Text Bundle
 		"T":  tbs.GetText,
 		"TF": tbs.Format,
@@ -20,4 +26,9 @@ func Functions() tpl.FuncMap {
 		"Select": tags.SelectRender,
 		"Pager":  tags.PagerRender,
 	}
+}
+
+func icon(icon string, clss ...string) template.HTML {
+	clss = append(clss, tbs.GetText("", "icon."+icon))
+	return template.HTML(`<i class="` + str.Join(clss, " ") + `"></i>`) //nolint: gosec
 }

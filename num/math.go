df9a1782b229e59af824c2a291722d7054c3fbc7
Frank Wang <yf.frank.wang@outlook.com>
2024-07-17 12:45:54 +0900

    num: add IfZero(), Abs()

--- num/math.go
+++ num/math.go
@@ -1,22 +1,49 @@
 package num
 
-func AbsInt(n int) int {
-	if n < 0 {
-		return -n
+// AbsInt returns the absolute value of x.
+func AbsInt(x int) int {
+	if x < 0 {
+		return -x
 	}
-	return n
+	return x
 }
 
-func AbsInt32(n int32) int32 {
-	if n < 0 {
-		return -n
+// AbsInt16 returns the absolute value of x.
+func AbsInt16(x int16) int16 {
+	if x < 0 {
+		return -x
 	}
-	return n
+	return x
 }
 
-func AbsInt64(n int64) int64 {
-	if n < 0 {
-		return -n
+// AbsInt32 returns the absolute value of x.
+func AbsInt32(x int32) int32 {
+	if x < 0 {
+		return -x
 	}
-	return n
+	return x
+}
+
+// AbsInt64 returns the absolute value of x.
+func AbsInt64(x int64) int64 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// AbsFloat32 returns the absolute value of x.
+func AbsFloat32(x float32) float32 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// AbsFloat64 returns the absolute value of x.
+func AbsFloat64(x float64) float64 {
+	if x < 0 {
+		return -x
+	}
+	return x
 }

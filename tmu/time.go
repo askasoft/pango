8698acd91ce30c61d4fa510639b8f985ed15a672
Frank Wang <yf.frank.wang@outlook.com>
2024-10-31 09:26:43 +0900

    tmu: add IsLeapYear(), AddYear(), AddMonth()

--- tmu/time.go
+++ tmu/time.go
@@ -59,3 +59,52 @@ func Parse(value string, layouts ...string) (tt time.Time, err error) {
 	}
 	return
 }
+
+func IsLeapYear(t time.Time) bool {
+	return t.YearDay() > 365
+}
+
+// AddYear(2020-02-29, 1) = 2021-02-28
+// AddYear(2020-02-29, 10) = 2030-02-28
+// AddYear(2020-02-29, 12) = 2032-02-29
+// AddYear(2020-02-29, -1) = 2019-02-28
+// AddYear(2020-02-29, -10) = 2010-02-28
+// AddYear(2020-02-29, -12) = 2008-02-29
+func AddYear(t time.Time, y int) time.Time {
+	n := t.AddDate(y, 0, 0)
+	if t.Month() != n.Month() {
+		n = n.AddDate(0, 0, -1)
+	}
+	return n
+}
+
+// AddMonth2019-12-31, 1) = 2020-01-31
+// AddMonth2019-12-31, 2) = 2020-02-29
+// AddMonth2019-12-31, 3) = 2020-03-31
+// AddMonth2019-12-31, 12) = 2020-12-31
+// AddMonth2019-12-31, 13) = 2021-01-31
+// AddMonth2019-12-31, 14) = 2021-02-28
+// AddMonth2020-03-31, -1) = 2020-02-29
+// AddMonth2020-03-31, -2) = 2020-01-31
+// AddMonth2020-03-31, -3) = 2019-12-31
+// AddMonth2020-03-31, -4) = 2019-11-30
+func AddMonth(t time.Time, m int) time.Time {
+	n := t
+
+	y := m / 12
+	m = m % 12
+
+	if m != 0 {
+		n = t.AddDate(0, m, 0)
+	}
+
+	if y != 0 {
+		n = AddYear(n, y)
+	}
+
+	if m != 0 && (int(t.Month())+m)%12 != int(n.Month()%12) {
+		n = n.AddDate(0, 0, -n.Day())
+	}
+
+	return n
+}

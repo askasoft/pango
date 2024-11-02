cae8eaff700881e5c44eaa725e305e5c066dbd95
Frank Wang <yf.frank.wang@outlook.com>
2024-11-02 18:52:05 +0900

    ref: add GetProperty()

--- ref/getsetter.go
+++ ref/getsetter.go
@@ -7,6 +7,41 @@ import (
 	"github.com/askasoft/pango/str"
 )
 
+func GetProperty(o any, k string) (v any, err error) {
+	if !IsPtrType(o) {
+		return nil, fmt.Errorf("%T is not a pointer", o)
+	}
+
+	defer func() {
+		if er := recover(); er != nil {
+			err = fmt.Errorf("GetProperty(%T, %q): %v", o, k, er)
+		}
+	}()
+
+	p := str.Capitalize(k)
+	r := reflect.ValueOf(o)
+
+	m := r.MethodByName("Get" + p)
+	if m.IsValid() && m.Type().NumIn() == 0 && (m.Type().NumOut() == 1 || m.Type().NumOut() == 2) {
+		rs := m.Call(nil)
+		v = rs[0].Interface()
+		if len(rs) == 2 {
+			if er, ok := rs[1].Interface().(error); ok {
+				err = er
+			}
+		}
+		return
+	}
+
+	f := r.Elem().FieldByName(p)
+	if f.IsValid() {
+		v = f.Interface()
+		return
+	}
+
+	return nil, fmt.Errorf("Missing property %q of %v", k, r.Type())
+}
+
 func SetProperty(o any, k string, v any) (err error) {
 	if !IsPtrType(o) {
 		return fmt.Errorf("%T is not a pointer", o)
@@ -14,7 +49,7 @@ func SetProperty(o any, k string, v any) (err error) {
 
 	defer func() {
 		if er := recover(); er != nil {
-			err = fmt.Errorf("SetProperty(%v, %q, %v): %v", o, k, v, er)
+			err = fmt.Errorf("SetProperty(%T, %q, %T): %v", o, k, v, er)
 		}
 	}()
 
---
+++ ref/getsetter_test.go
@@ -0,0 +1,51 @@
+package ref
+
+import (
+	"reflect"
+	"testing"
+)
+
+type testObject struct {
+	Int      int
+	String   string
+	Bool     bool
+	StrSlice []string
+}
+
+func TestSetProperty(t *testing.T) {
+	cs := []struct {
+		o *testObject
+		k string
+		v any
+		w any
+		g func(o *testObject) any
+	}{
+		{&testObject{}, "Int", "1", 1, func(o *testObject) any { return o.Int }},
+		{&testObject{}, "String", true, "true", func(o *testObject) any { return o.String }},
+		{&testObject{}, "StrSlice", nil, []string{}, func(o *testObject) any { return o.StrSlice }},
+	}
+
+	for i, c := range cs {
+		err := SetProperty(c.o, c.k, c.v)
+		if err != nil {
+			t.Errorf("[%d] SetProperty(%s, %v) = %v", i, c.k, c.v, err)
+			continue
+		}
+
+		a := c.g(c.o)
+		if !reflect.DeepEqual(c.w, a) {
+			t.Errorf("[%d] SetProperty(%s, %v) get %v, want %v", i, c.k, c.v, a, c.w)
+			continue
+		}
+
+		g, err := GetProperty(c.o, c.k)
+		if err != nil {
+			t.Errorf("[%d] GetProperty(%s) = %v", i, c.k, err)
+			continue
+		}
+		if !reflect.DeepEqual(c.w, g) {
+			t.Errorf("[%d] GetProperty(%s) = %v, want %v", i, c.k, a, c.w)
+			continue
+		}
+	}
+}

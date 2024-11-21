9049e24888d4cbd1bceac1618b748e5c8844e4d5
Frank Wang <yf.frank.wang@outlook.com>
2024-11-21 13:21:34 +0900

    xin: remove TestUnixSocket() due to 'connect: no such file or directory' error

--- xin/xin_integration_linux_test.go
+++ xin/xin_integration_linux_test.go
@@ -6,69 +6,11 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/askasoft/pango/ran"
 	"github.com/stretchr/testify/assert"
 )
-
-// testRunUnix attaches the router to a http.Server and starts listening and serving HTTP requests
-// through the specified unix socket (i.e. a file).
-// Note: this method will block the calling goroutine indefinitely unless an error happens.
-func testRunUnix(engine *Engine, file string) (err error) {
-	engine.Logger.Infof("Listening and serving HTTP on unix:/%s", file)
-
-	var listener net.Listener
-	listener, err = net.Listen("unix", file)
-	if err != nil {
-		engine.Logger.Errorf("Listen on unix:/%s failed: %v", file, err)
-		return
-	}
-	defer listener.Close()
-	defer os.Remove(file)
-
-	server := testNewHttpServer(engine)
-	err = server.Serve(listener)
-	if err != nil {
-		engine.Logger.Errorf("Serve on unix:/%s failed: %v", file, err)
-	}
-	return
-}
-
-func TestUnixSocket(t *testing.T) {
-	router := New()
-
-	unixTestSocket := filepath.Join(os.TempDir(), "xin_unix_test_"+ran.RandNumbers(8))
-
-	defer os.Remove(unixTestSocket)
-
-	go func() {
-		router.GET("/example", func(c *Context) { c.String(http.StatusOK, "it worked") })
-		assert.NoError(t, testRunUnix(router, unixTestSocket))
-	}()
-	// have to wait for the goroutine to start and run the server
-	// otherwise the main thread will complete
-	time.Sleep(5 * time.Millisecond)
-
-	c, err := net.Dial("unix", unixTestSocket)
-	assert.NoError(t, err)
-
-	fmt.Fprint(c, "GET /example HTTP/1.0\r\n\r\n")
-	scanner := bufio.NewScanner(c)
-	var response string
-	for scanner.Scan() {
-		response += scanner.Text()
-	}
-	assert.Contains(t, response, "HTTP/1.0 200", "should get a 200")
-	assert.Contains(t, response, "it worked", "resp body should match")
-}
-
-func TestBadUnixSocket(t *testing.T) {
-	router := New()
-	assert.Error(t, testRunUnix(router, "#/tmp/unix_unit_test"))
-}
 
 // testRunFd attaches the router to a http.Server and starts listening and serving HTTP requests
 // through the specified file descriptor.

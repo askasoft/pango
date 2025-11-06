c9795f6fedfd1a9972bffb99255a89368facedae
Frank Wang <yf.frank.wang@outlook.com>
2025-11-06 09:53:08 +0900

    str: move Clone() from strings_go118.go to strings.go

--- str/strings.go
+++ str/strings.go
@@ -25,6 +25,20 @@ func NewReplacer(oldnew ...string) *Replacer {
 	return strings.NewReplacer(oldnew...)
 }
 
+// Clone returns a fresh copy of s.
+// It guarantees to make a copy of s into a new allocation,
+// which can be important when retaining only a small substring
+// of a much larger string. Using Clone can help such programs
+// use less memory. Of course, since using Clone makes a copy,
+// overuse of Clone can make programs use more memory.
+// Clone should typically be used only rarely, and only when
+// profiling indicates that it is needed.
+// For strings of length zero the string "" will be returned
+// and no allocation is made.
+func Clone(s string) string {
+	return strings.Clone(s)
+}
+
 // Compare returns an integer comparing two strings lexicographically.
 // The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
 //

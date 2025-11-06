b6ab0d400aa69de268793d691516ef6adf3840c3
Frank Wang <yf.frank.wang@outlook.com>
2025-11-06 10:02:43 +0900

    str: use go1.20 unsafe.Slice()

--- str/unsafe.go
+++ str/unsafe.go
@@ -6,10 +6,10 @@ import (
 
 // UnsafeBytes returns a []byte from string with no memory allocations
 func UnsafeBytes(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(
-		&struct {
-			string
-			Cap int
-		}{s, len(s)},
-	))
+	return unsafe.Slice(unsafe.StringData(s), len(s))
+}
+
+// UnsafeString returns a string from []byte with no memory allocations
+func UnsafeString(bs []byte) string {
+	return unsafe.String(unsafe.SliceData(bs), len(bs))
 }
--- str/unsafe_go119.go
+++ str/unsafe_go119.go
@@ -1,5 +1,4 @@
 //go:build !go1.20
-// +build !go1.20
 
 package str
 
@@ -10,4 +9,14 @@ import (
 // UnsafeString returns a string from []byte with no memory allocations
 func UnsafeString(bs []byte) string {
 	return *(*string)(unsafe.Pointer(&bs))
+}
+
+// UnsafeBytes returns a []byte from string with no memory allocations
+func UnsafeBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
 }
